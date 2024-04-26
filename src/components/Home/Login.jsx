43a1b37308c7644e1512f802a6c8bec25f5a47fa
SayaliNikumbh25 <sayalinikumbh7890@gmail.com>
2024-04-26 11:21:15 +0530

    boilerplate react setup

---
+++ src/components/Home/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <div>Login</div>
+        <div>
+            <form action="">
+                <input type="text" name="name" id="name" placeholder='Enter Name'/>
+                <input type="email" name="email" id="email" placeholder=''/>
+            </form>
+        </div>
+    </>
+    
+  )
+}
+
+export default Login
